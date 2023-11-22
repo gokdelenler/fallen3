2eebe7ac995131a66d81ab2cbb336b7c3cea5169
gokdelenler <106874399+gokdelenler@users.noreply.github.com>
2023-11-22 19:14:34 +0300

    Update __main__.py

--- FallenMusic/__main__.py
+++ FallenMusic/__main__.py
@@ -58,7 +58,7 @@ async def fallen_startup():
     try:
         await app.send_message(
             SUNAME,
-            f"✯ ғᴀʟʟᴇɴ ᴍᴜsɪᴄ ʙᴏᴛ ✯\n\n𖢵 ɪᴅ : `{BOT_ID}`\n𖢵 ɴᴀᴍᴇ : {BOT_NAME}\n𖢵 ᴜsᴇʀɴᴀᴍᴇ : @{BOT_USERNAME}",
+            f"✯ efsane müzik botu ✯\n\n𖢵 ɪᴅ : `{BOT_ID}`\n𖢵 isim : {BOT_NAME}\n𖢵 kulanıcı adı : @{BOT_USERNAME}",
         )
     except:
         LOGGER.error(
@@ -68,7 +68,7 @@ async def fallen_startup():
     try:
         await app2.send_message(
             SUNAME,
-            f"✯ ғᴀʟʟᴇɴ ᴍᴜsɪᴄ ᴀss ✯\n\n𖢵 ɪᴅ : `{ASS_ID}`\n𖢵 ɴᴀᴍᴇ : {ASS_NAME}\n𖢵 ᴜsᴇʀɴᴀᴍᴇ : @{ASS_USERNAME}",
+            f"✯ efsane müzik ass ✯\n\n𖢵 ɪᴅ : `{ASS_ID}`\n𖢵 isim : {ASS_NAME}\n𖢵 kulanıcı adı : @{ASS_USERNAME}",
         )
     except:
         LOGGER.error(
