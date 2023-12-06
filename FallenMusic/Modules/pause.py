5e521bd666eee51e0218765a52018963157c4888
gokdelenler <106874399+gokdelenler@users.noreply.github.com>
2023-12-06 11:17:35 +0300

    Update pause.py

--- FallenMusic/Modules/pause.py
+++ FallenMusic/Modules/pause.py
@@ -37,12 +37,12 @@ async def pause_str(_, message: Message):
 
     if not await is_streaming(message.chat.id):
         return await message.reply_text(
-            "ᴅɪᴅ ʏᴏᴜ ʀᴇᴍᴇᴍʙᴇʀ ᴛʜᴀᴛ ʏᴏᴜ ʀᴇsᴜᴍᴇᴅ ᴛʜᴇ sᴛʀᴇᴀᴍ ?"
+            "akışın devam etiğini hatırlıyormusun ?"
         )
 
     await pytgcalls.pause_stream(message.chat.id)
     await stream_off(message.chat.id)
     return await message.reply_text(
-        text=f"➻ sᴛʀᴇᴀᴍ ᴩᴀᴜsᴇᴅ 🥺\n│ \n└ʙʏ : {message.from_user.mention} 🥀",
+        text=f"➻ akış duraklatıldı 🥺\n│ \n└ʙʏ : {message.from_user.mention} 🥀",
         reply_markup=close_key,
     )
